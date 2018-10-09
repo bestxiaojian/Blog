7cb1a34ba40458eea124f12e0205ba5bbd7a0763
Wang Jian <wangjian10@le.com>
2018-10-09 11:52:06 +0800

    Update for write2mongo.sh,add delete operation
    Signed-off-by: Wang Jian <wangjian10@le.com>

--- code/mongo/write2mongo.sh
+++ code/mongo/write2mongo.sh
@@ -2,7 +2,8 @@
 
 # mongoimport --port 27030 -u sa -p Expressin@0618 -d mapdb -c bike_bak  --type=json --file bike.csv
 # mongo config
-mongoPath=/data/wangjian10_baseline/extra_home/wangjian10/mongodb/mongodb-linux-x86_64-rhel62-3.4.17/bin/mongoimport
+mongoimportPath=/data/wangjian10_baseline/extra_home/wangjian10/mongodb/mongodb-linux-x86_64-rhel62-3.4.17/bin/mongoimport
+mongoPath=/data/wangjian10_baseline/extra_home/wangjian10/mongodb/mongodb-linux-x86_64-rhel62-3.4.17/bin/mongo
 mongoHost=127.0.0.1
 mongoPort=27017
 mongoDB=testdb
@@ -10,9 +11,16 @@ mongoUser=work
 mongoPSW=work
 monogoTable=kideducation
 
-mongoimportStr=$mongoPath" --host "$mongoHost" --port "$mongoPort" -u "$mongoUser" -p "$mongoPSW" -d "$mongoDB" -c "$monogoTable" --type=json --file "$1
-#echo $mongoimportStr
+mongoimportStr=$mongoimportPath" --host "$mongoHost" --port "$mongoPort" -u "$mongoUser" -p "$mongoPSW" -d "$mongoDB" -c "$monogoTable" --type=json --file "$1
+mongocmdStr=$mongoPath" "$mongoHost":"$mongoPort"/"$mongoDB" -u"$mongoUser" -p"$mongoPSW" --eval "
+deleteKidedu="db.$monogoTable.drop()"
+deletecmd=$mongocmdStr$deleteKidedu
 
-#mongoimport data to mongodb
-$mongoimportStr
+if [ $1 = delete ]
+then
+	$deletecmd
+else
+	$mongoimportStr
+fi
+
 
